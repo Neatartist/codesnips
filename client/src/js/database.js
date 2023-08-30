74db30f5e433a8dccfa7b3f28bc8c51d525b9f6c
Neatartist <davk0810@gmail.com>
2023-08-29 23:36:39 -0400

    update

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,15 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database 
-export const putDb = async (content) => console.error('putDb not implemented');
-export const deleteDb = async (id) => console.error('deleteDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  store.put(content);
+  return tx.complete;
+}
+console.error('putDb not implemented');
+
 
 
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-const db = getDb();
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  return store.getAll();
+}
+console.error('getDb not implemented') 
 
 
 initdb();

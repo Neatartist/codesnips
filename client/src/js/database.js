64f9d54570d619e2896ac8d334ddadbe29c089ff
Neatartist <davk0810@gmail.com>
2023-08-29 23:27:57 -0400

    upload

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,12 +12,16 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-
+// TODO: Add logic to a method that accepts some content and adds it to the database 
 export const putDb = async (content) => console.error('putDb not implemented');
+export const deleteDb = async (id) => console.error('deleteDb not implemented');
+
+
 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+const db = getDb();
+
 
 initdb();
